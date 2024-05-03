6e5bad074e0c4c6a745c1a37c22bfc238abb6be0
„Teresa <teresakramek@interia.pl>
2024-05-03 20:45:23 +0200

    Change gitignore

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,4 +1,7 @@
 from fastapi import FastAPI
+import requests
+import json
+from datetime import datetime, date
 
 app = FastAPI()
 
@@ -8,6 +11,24 @@ def hello_world():
     return {"message": "OK"}
 
 
-@app.get("/api")
-def hello_world():
-    return {"message": "Needs to be implemented"}
+@app.get("/api/exchange-rate/{currency}")
+def currencies(currency: str, date_from: str | None = None):
+    # get from and to dates - requires
+    if date_from is None:
+        date_from = date.today().strftime('%Y-%m-%d')
+        print(date_from)
+    
+    response = requests.get(f"https://api.nbp.pl/api/exchangerates/rates/a/{currency}/{date_from}?format=json")
+
+    if response.status_code == 200:
+        return response.json()
+
+    return {
+        "error": f"cannot fetch rates for {currency}",
+        "code": response.status_code
+        }
+
+
+@app.get("/api/currencies")
+def currencies():
+    return {'currencies': ['USD', 'EUR', 'GPB']}
