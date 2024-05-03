47d082de85286d68b55698e3c4e0bd0cb26b83f8
„Teresa <teresakramek@interia.pl>
2024-05-03 11:39:52 +0200

    clean compose files, change command to run API with reload and mount for dev

--- backend/app/main.py
+++ backend/app/main.py
@@ -2,7 +2,12 @@ from fastapi import FastAPI
 
 app = FastAPI()
 
-
+# healthcheck
 @app.get("/")
 def hello_world():
     return {"message": "OK"}
+
+
+@app.get("/api")
+def hello_world():
+    return {"message": "Needs to be implemented"}
