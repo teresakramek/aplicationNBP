f433013d86ecbcda704b2991706f03caa9913769
„Teresa <teresakramek@interia.pl>
2024-05-07 19:21:02 +0200

    Add configuration database

--- backend/app/database.py
+++ backend/app/database.py
@@ -7,6 +7,7 @@ SQLALCHEMY_DATABASE_URL = "postgresql://NBP_admin:05Qwerty33#2024@db/aplicationN
 engine = create_engine(
     SQLALCHEMY_DATABASE_URL
 )
+
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
 Base = declarative_base()
